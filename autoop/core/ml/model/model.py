69b542d56c4bfb5c17b61d625eb272ab61bca19c
Nathan <n.r.veenstra.1@student.rug.nl>
2024-11-03 15:39:09 +0100

    model changes

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -1,15 +1,21 @@
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from autoop.core.ml.artifact import Artifact
 import numpy as np
 from copy import deepcopy
 from typing import Literal
 
-class Model(Artifact):
-    def __init__(self, *args, **kwargs):
-        super().__init__(type="model", *args, **kwargs)
-        
+class Model(ABC):
+    """Base class for all models.
+    """
+    def __init__(self):
+        self._parameters = {}
+
     @abstractmethod
-    def predict(self, data: np.ndarray) -> np.ndarray:
+    def fit(self, dataset: Artifact):
+        pass
+    
+    @abstractmethod
+    def predict(self, dataset: Artifact) -> np.ndarray:
         pass
     
